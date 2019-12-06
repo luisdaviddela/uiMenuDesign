0b19830451282a5e683a3295ac8aba2b44c37d03
David De La Cruz <delacruz_luis@outlook.es>
2019-12-06 17:35:39 -0600

    FullMenu

---
+++ uiMenu/uiMenu/uiMenu/View/MainMenuView.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace uiMenu
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainMenuView : ContentPage
+	{
+        List<DTO_MenuCard> info = new List<DTO_MenuCard>();
+        private static readonly HttpClient client = new HttpClient();
+        public MainMenuView ()
+		{
+			InitializeComponent ();
+		}
+        protected async override void OnAppearing()
+        {
+            var responseString = await client.GetStringAsync("");
+            string resp = Convert.ToString(responseString);
+            var obj = JsonConvert.DeserializeObject<object>(resp);
+            string data = Convert.ToString(obj);
+            info = JsonConvert.DeserializeObject<List<DTO_MenuCard>>(data);
+            if (info.Count>0)
+            {
+                VM_TemplateMainMenu AppBinding = new VM_TemplateMainMenu(info);
+                listView.ItemsSource = AppBinding.CardDataCollection;
+            }
+            base.OnAppearing(); 
+        }
+        public class DTO_MenuCard
+        {
+            public int IdMenu { get; set; }
+            public string Titulo { get; set; }
+            public string Subitulo { get; set; }
+            public string CardIcon { get; set; }
+            public string AlertColor { get; set; }
+        }
+        private async void EvetClicked(object s, SelectedItemChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ uiMenu/uiMenu/uiMenu/ViewModel/VM_TemplateMainMenu.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Xamarin.Forms;
+namespace uiMenu
+{
+    class VM_TemplateMainMenu
+    {
+        
+        public IList<CardDaraModel> CardDataCollection { get; set; }
+        public object SelectedItem { get; set; }
+        private static readonly HttpClient client = new HttpClient();
+        public VM_TemplateMainMenu(List<MainMenuView.DTO_MenuCard> info)
+        {
+            CardDataCollection = new List<CardDaraModel>();
+            GenerateCardModel(infoMenu:info);
+        }
+        public void GenerateCardModel(List<MainMenuView.DTO_MenuCard> infoMenu)
+        {
+            foreach (var item in infoMenu)
+            {
+                
+                var cardDataAprobaciones = new CardDaraModel()
+                {
+                    IdView = item.IdMenu,
+                    Cantidad = Convert.ToString(100),
+                    Color = Color.Aqua,
+                    Title = item.Titulo,
+                    Owner = item.Subitulo,
+                    CardIcon = "cover_medium.JPG",
+                    AlertColor = Color.FromHex("#C0392B")
+                };
+                CardDataCollection.Add(cardDataAprobaciones);
+            }
+        }
+    }
+    
+    class CardDaraModel
+    {
+        public int IdView { get; set; }
+        public string Cantidad { get; set; }
+        public string Title { get; set; }
+        public Color Color { get; set; }
+        public string Owner { get; set; }
+        public string CardIcon { get; set; }
+        public Color AlertColor { get; set; }
+    }
+}
